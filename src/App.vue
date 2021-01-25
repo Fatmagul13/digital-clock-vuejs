923228f035f9066f20e805c094edbc96a342ad81
Fatmagul13 <yurtsevenfatmagul@gmail.com>
2021-01-26 00:29:02 +0300

    digital clock

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="col-md-4 col-md-offset-4" id="clock">
+    <h1>Digital Clock with Vue.js</h1>
+    <p id="time">{{ time }}</p>
+
+    <p id="date">{{ date }}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      time: "",
+      date: "",
+    };
+  },
+  methods: {
+    zeroPadding(num, digit) {
+      var zero = "";
+      for (var i = 0; i < digit; i++) {
+        zero += "0";
+      }
+      return (zero + num).slice(-digit);
+    },
+    updateTime() {
+      var week = ["SUN", "MON", "TUE", "WED", "THU", "FRİ", "SAT"];
+      var date = new Date();
+      this.time =
+        this.zeroPadding(date.getHours(), 2) +
+        ":" +
+        this.zeroPadding(date.getMinutes(), 2) +
+        ":" +
+        this.zeroPadding(date.getSeconds(), 2);
+      this.date =
+        this.zeroPadding(date.getFullYear(), 4) +
+        "-" +
+        this.zeroPadding(date.getMonth() + 1, 2) +
+        "-" +
+        this.zeroPadding(date.getDate(), 2) +
+        " " +
+        week[date.getDay()];
+    },
+  },
+  mounted() {
+    setInterval(() => {
+      this.updateTime();
+      console.log("timeout");
+    }, 1000);
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: "Share Tech Mono", monospace;
+}
+body {
+  background-color:#17202A;
+  color: #fff;
+}
+#clock {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%) translateY(-50%);
+  color: #D5D8DC;
+  letter-spacing: 7px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+#time {
+  font-size: 50px;
+}
+#date {
+  font-size: 20px;
 }
 </style>
